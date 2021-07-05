fc2b92b04df5ea55a8ed09bacea39eb785405bdc
K-J-HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2021-07-05 20:31:16 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 'use strict';
-
 // arrow function 'scroll', () => {};
-
 
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
@@ -15,3 +13,18 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');   
     }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView( {behavior: 'smooth'});
+})
+
