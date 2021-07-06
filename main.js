5c59e2375c97222168e744303f439261c676ec1b
K-J-HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2021-07-06 17:00:49 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -38,10 +38,27 @@ homeContactBtn.addEventListener('click', () => {
 });
 
 
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    // console.log(homeHeight);
+    // console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+
 
 
 
